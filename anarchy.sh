e1043c0ed898cd1ede42c65adb463d08af9f89c2
Henrique Leal <henrique@syntesis.com.br>
2013-01-21 09:42:19 -0200

    Translate message and comments for english

--- anarchy.sh
+++ anarchy.sh
@@ -3,57 +3,60 @@
 
 
 #################################################
-#		     Atenção			#
-#						#
+#            Warning                            #
+#                                               #
 #  This is script is TOTALLY outdated. We are   #
 # working hard to fix them and put this project #
-# up and running.				#
-#						#
+# up and running.                               #
+#                                               #
 #  If you're willing to help, please submit and #
-# push request. 				#
-#						#
-# Happy hacking and long live Rock n' Roll	#
-#						#
+# push request.                                 #
+#                                               #
+# Happy hacking and long live Rock n' Roll      #
+#                                               #
 #################################################
 
 
 
 ##################################################
-#		    Variaveis 			 #
+#           Variables                            #
 ##################################################
-# Nome do Computador
+# Hostname
 HOSTN=Arch
 
-# Localização. Verifique o diretório /usr/share/zoneinfo/<Zone>/<SubZone>
+# Time zone
+# Available time zones and subzones can be found in the /usr/share/zoneinfo/<Zone>/<SubZone> directories. 
 LOCALE=America/Sao_Paulo
 
-# Senha de Root do sistema após a instalação
+# Root password
 ROOT_PASSWD=123
 
-########## Variáveis Para Particionamento do Disco
-# ATENÇÃO, este script apaga TODO o conteúdo do disco especificado em $HD.
+########## Variables To Disk Partitioning
+# WARNING, this script delete ALL the contents of the disc specified in $ HD.
 HD=/dev/sda
-# Tamanho da Partição Boot: /boot
+# Boot partition size: /boot
 BOOT_SIZE=200
-# Tamanho da Partição Root: /
+# Root partition size: /
 ROOT_SIZE=10000
-# Tamanho da Partição Swap:
+# Swap partition size:
 SWAP_SIZE=2000
-# A partição /home irá ocupar o restante do espaço livre em disco
+# The / home partition will occupy the remaining free disk space
 
-# File System das partições
+# File Systems
 BOOT_FS=ext4
 HOME_FS=ext4
 ROOT_FS=ext4
 
-# Pacote extras (não são obrigatórios)
+# Extra packages (not required)
 EXTRA_PKGS='vim'
 
-######## Variáveis menos suscetíveis a mudanças
+# Keyboard Layout
 KEYBOARD_LAYOUT=br-abnt2
+
+# Language
 LANGUAGE=pt_BR
 
-######## Variáveis auxiliares. NÃO DEVEM SER ALTERADAS
+######## Auxiliary variables. SHOULD NOT BE CHANGED
 BOOT_START=1
 BOOT_END=$(($BOOT_START+$BOOT_SIZE))
 
@@ -66,176 +69,176 @@ ROOT_END=$(($ROOT_START+$ROOT_SIZE))
 HOME_START=$ROOT_END
 
 ##################################################
-#		    functions 			 #
+#           functions                            #
 ##################################################
 function inicializa_hd
 {
-	echo "Inicializando o HD"
-	# Configura o tipo da tabela de partições (Ignorando erros)
-	parted -s $HD mklabel msdos &> /dev/null
+    echo "Initializing HD"
+    # Setting the type of partition table (Skipping errors)
+    parted -s $HD mklabel msdos &> /dev/null
 
-	# Remove qualquer partição antiga
-	parted -s $HD rm 1 &> /dev/null
-	parted -s $HD rm 2 &> /dev/null
-	parted -s $HD rm 3 &> /dev/null
-	parted -s $HD rm 4 &> /dev/null
+    # Remove ALL partitions
+    parted -s $HD rm 1 &> /dev/null
+    parted -s $HD rm 2 &> /dev/null
+    parted -s $HD rm 3 &> /dev/null
+    parted -s $HD rm 4 &> /dev/null
 }
 
 function particiona_hd
 {
-	ERR=0
-	# Cria partição boot
-	echo "Criando partição boot"
-	parted -s $HD mkpart primary $BOOT_FS $BOOT_START $BOOT_END 1>/dev/null || ERR=1
-	parted -s $HD set 1 boot on 1>/dev/null || ERR=1
+    ERR=0
+    # Create Boot partition
+    echo "Creating Boot partition"
+    parted -s $HD mkpart primary $BOOT_FS $BOOT_START $BOOT_END 1>/dev/null || ERR=1
+    parted -s $HD set 1 boot on 1>/dev/null || ERR=1
 
-	# Cria partição swap
-	echo "Criando partição swap"
-	parted -s $HD mkpart primary linux-swap $SWAP_START $SWAP_END 1>/dev/null || ERR=1
+    # Create Swap partition
+    echo "Creating Swap partition"
+    parted -s $HD mkpart primary linux-swap $SWAP_START $SWAP_END 1>/dev/null || ERR=1
 
-	# Cria partição root
-	echo "Criando partição root"
-	parted -s $HD mkpart primary $ROOT_FS $ROOT_START $ROOT_END 1>/dev/null || ERR=1
+    # Create Root partition
+    echo "Creating Root partition"
+    parted -s $HD mkpart primary $ROOT_FS $ROOT_START $ROOT_END 1>/dev/null || ERR=1
 
-	# Cria partição home
-	echo "Criando partição home"
-	parted -s -- $HD mkpart primary $HOME_FS $HOME_START -0 1>/dev/null || ERR=1
+    # Create Home partition
+    echo "Creating Home partition"
+    parted -s -- $HD mkpart primary $HOME_FS $HOME_START -0 1>/dev/null || ERR=1
 
-	if [[ $ERR -eq 1 ]]; then
-		echo "Erro durante o particionamento"
-		exit 1
-	fi
+    if [[ $ERR -eq 1 ]]; then
+        echo "Partition error"
+        exit 1
+    fi
 }
 
 function cria_fs
 {
-	ERR=0
-	# Formata partições root, home e boot para o File System especificado
-	echo "Formatando partição boot"
-	mkfs.$BOOT_FS /dev/sda1 -L Boot 1>/dev/null || ERR=1
-	echo "Formatando partição root"
-	mkfs.$ROOT_FS /dev/sda3 -L Root 1>/dev/null || ERR=1
-	echo "Formatando partição home"
-	mkfs.$HOME_FS /dev/sda4 -L Home 1>/dev/null || ERR=1
-	# Cria e inicia a swap
-	echo "Formatando partição swap"
-	mkswap /dev/sda2 || ERR=1
-	swapon /dev/sda2 || ERR=1
+    ERR=0
+    # Formats root, boot and home partitions to the specified File System
+    echo "Formatting Boot partition"
+    mkfs.$BOOT_FS /dev/sda1 -L Boot 1>/dev/null || ERR=1
+    echo "Formatting Root partitiont"
+    mkfs.$ROOT_FS /dev/sda3 -L Root 1>/dev/null || ERR=1
+    echo "Formatting Home partition"
+    mkfs.$HOME_FS /dev/sda4 -L Home 1>/dev/null || ERR=1
+    # Create and initializes the swap
+    echo "Formatting Swap partition"
+    mkswap /dev/sda2 || ERR=1
+    swapon /dev/sda2 || ERR=1
 
-	if [[ $ERR -eq 1 ]]; then
-		echo "Erro ao criar File Systems"
-		exit 1
-	fi
+    if [[ $ERR -eq 1 ]]; then
+        echo "File Systems error"
+        exit 1
+    fi
 }
 
 function monta_particoes
 {
-	ERR=0
-	echo "Montando partições"
-	# Monta partição root
-	mount /dev/sda3 /mnt || ERR=1
-	# Monta partição boot
-	mkdir /mnt/boot || ERR=1
-	mount /dev/sda1 /mnt/boot || ERR=1
-	# Monta partição home
-	mkdir /mnt/home || ERR=1
-	mount /dev/sda4 /mnt/home || ERR=1
+    ERR=0
+    echo "Mounting partitions"
+    # Mount Root partition
+    mount /dev/sda3 /mnt || ERR=1
+    # Mount Boot partition
+    mkdir /mnt/boot || ERR=1
+    mount /dev/sda1 /mnt/boot || ERR=1
+    # Mount Home partition
+    mkdir /mnt/home || ERR=1
+    mount /dev/sda4 /mnt/home || ERR=1
 
-	if [[ $ERR -eq 1 ]]; then
-		echo "Erro ao criar File Systems"
-		exit 1
-	fi
+    if [[ $ERR -eq 1 ]]; then
+        echo "Mounting error"
+        exit 1
+    fi
 }
 
 function configurando_pacman
 {
-	echo "Configurando pacman"
-	cp /etc/pacman.d/mirrorlist /etc/pacman.d/mirrorlist.bkp
-	sed -i "s/^Ser/#Ser/" /etc/pacman.d/mirrorlist
-	sed -i '/Brazil/{n;s/^#//}' /etc/pacman.d/mirrorlist
+    echo "Setting Pacman"
+    cp /etc/pacman.d/mirrorlist /etc/pacman.d/mirrorlist.bkp
+    sed -i "s/^Ser/#Ser/" /etc/pacman.d/mirrorlist
+    sed -i '/Brazil/{n;s/^#//}' /etc/pacman.d/mirrorlist
 
-	if [ "$(uname -m)" = "x86_64" ]
-	then
-		cp /etc/pacman.conf /etc/pacman.conf.bkp
-		# Adiciona o Multilib 
-		sed -i '/^#\[multilib\]/{s/^#//;n;s/^#//;n;s/^#//}' /etc/pacman.conf
+    if [ "$(uname -m)" = "x86_64" ]
+    then
+        cp /etc/pacman.conf /etc/pacman.conf.bkp
+        # Add Multilib 
+        sed -i '/^#\[multilib\]/{s/^#//;n;s/^#//;n;s/^#//}' /etc/pacman.conf
 
-	fi
+    fi
 }
 
 function instalando_sistema
 {
-	ERR=0
-	echo "Rodando pactrap base base-devel"
-	pacstrap /mnt base base-devel || ERR=1
-	echo "Rodando pactrap grub-bios $EXTRA_PKGS"
-	pacstrap /mnt grub-bios `echo $EXTRA_PKGS` || ERR=1
-	echo "Rodando genfstab"
-	genfstab -p /mnt >> /mnt/etc/fstab || ERR=1
+    ERR=0
+    echo "Running pacstrap base base-devel"
+    pacstrap /mnt base base-devel || ERR=1
+    echo "Running pacstrap grub-bios $EXTRA_PKGS"
+    pacstrap /mnt grub-bios `echo $EXTRA_PKGS` || ERR=1
+    echo "Running genfstab"
+    genfstab -p /mnt >> /mnt/etc/fstab || ERR=1
 
-	if [[ $ERR -eq 1 ]]; then
-		echo "Erro ao instalar sistema"
-		exit 1
-	fi
+    if [[ $ERR -eq 1 ]]; then
+        echo "Install error"
+        exit 1
+    fi
 }
 
 ##################################################
-#		    Script 			 #
+#           Script                               #
 ##################################################
-# Carrega layout do teclado ABNT2
+# Load Keybord Layout
 loadkeys $KEYBOARD_LAYOUT
 
-#### Particionamento
+#### Partitioning
 inicializa_hd
 particiona_hd
 cria_fs
 monta_particoes
 
-#### Instalação
+#### Installing
 configurando_pacman
 instalando_sistema
 
-#### Entra no novo sistema (chroot)
+#### Chroot and configure the base system
 arch-chroot /mnt << EOF
-# Configura hostname
+# Hostname
 echo $HOSTN > /etc/hostname
 cp /etc/hosts /etc/hosts.bkp
 sed -i 's/localhost$/localhost '$HOSTN'/' /etc/hosts
 
-# Configura layout do teclado
+# Keybord Layout
 echo 'KEYMAP='$KEYBOARD_LAYOUT > /etc/vconsole.conf
 echo 'FONT=lat0-16' >> /etc/vconsole.conf
 echo 'FONT_MAP=' >> /etc/vconsole.conf
 
-# Configura locale.gen
+# Locale locale.gen
 cp /etc/locale.gen /etc/locale.gen.bkp
 sed -i 's/^#'$LANGUAGE'/'$LANGUAGE/ /etc/locale.gen
 locale-gen
 
-# Configura locale.conf
+# Locale locale.conf
 export LANG=$LANGUAGE'.utf-8'
 echo 'LANG='$LANGUAGE'.utf-8' > /etc/locale.conf
 echo 'LC_COLLATE=C' >> /etc/locale.conf
 echo 'LC_TIME='$LANGUAGE'.utf-8' >> /etc/locale.conf
 
-# Configura hora
+# Time zone
 ln -s /usr/share/zoneinfo/$LOCALE /etc/localtime
 echo $LOCALE > /etc/timezone
 hwclock --systohc --utc
 
-# Configura rede (DHCP via eth0)
+# Setting Network (DHCP via eth0)
 # systemctl enable dhcpcd@eth0.service
 # Thanks to SystemD latest update the interfaces are now
 # receiveing unpredictable names
 
-# Configura ambiente ramdisk inicial
+# Create an initial ramdisk environment
 mkinitcpio -p linux
 
-# Instala e gera configuração do GRUB Legacy
+# Install and setting up GRUB Legacy
 grub-install /dev/sda
 grub-mkconfig -o /boot/grub/grub.cfg
 
-# Altera a senha do usuário root
+# Setting root password
 echo -e $ROOT_PASSWD"\n"$ROOT_PASSWD | passwd
 EOF
 
